1572ada7ffd5ee7dcb5c7efd741b8c5c9978a135
Maria sophia Mascardo <mariasophiamascardo@Pias-MacBook-Air.local>
2024-11-15 08:42:10 +0100

    Solved lab

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,11 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it("should return undefined if any of the two arguments is not a number", () =>{
+        expect(add({})).toBeUndefined();
+      expect(add([])).toBeUndefined();
+      expect(add(null)).toBeUndefined();
+        })
     });
   });
   
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,27 @@
-// UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(divide).toBeDefined();
+          });
+          it("should take two numbers arguments", () => {
+            expect(divide.length).toBe(2);
+          });
+          it("should return the division of the two numbers", () => {
+            expect(divide(4, 2)).toBe(2);
+            expect(divide(6, 3)).toBe(2);
+            expect(divide(100, 20)).toBe(5);
+          });
+          it("should return undefined if any of the arguments is not provided", () =>{
+            expect(divide({})).toBeUndefined();
+          expect(divide([])).toBeUndefined();
+          expect(divide(null)).toBeUndefined();
+            })
 
     })    
 })
 
-*/
